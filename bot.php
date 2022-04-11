abcf5b37ae85a69e758f8d90dd31b15b6c97c5f7
tecnologia <77143340+tecnologia733@users.noreply.github.com>
2022-04-11 20:30:31 -0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -5,6 +5,6 @@ $message    = $_POST[“message”];
 $phone      = $_POST["phone"];
 $group_name = $_POST["group_name"];
 
-$response = array("reply" => "Hello $sender, we received your message $message.");
+$response = array("reply" => "Oi $sender, recebemos a sua mensagem: $message.");
 echo json_encode($response);
 ?>
